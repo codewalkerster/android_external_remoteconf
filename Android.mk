e19352bad983746a569696da78a5ac03340cfc51
Li Geng <lig@lig-Ubuntu910.(none)>
2011-01-04 16:56:59 +0800

    Modify link to static

--- Android.mk
+++ Android.mk
@@ -5,6 +5,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= irremote.c config.c parsefile.c
 LOCAL_MODULE := remotecfg
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libcutils libc
 include $(BUILD_EXECUTABLE)
 
